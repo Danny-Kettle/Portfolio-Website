0dd26354b88dbe40f768ea0f7e99f9a5d1eade86
Danny Kettle <danny.kettle@dkwebservices.co.uk>
2023-06-05 16:44:05 +0100

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { DataContextProvider } from "./components/DataContextProvider";
 import Upcoming from "./components/Upcoming";
 import Home from "./pages/Home";
